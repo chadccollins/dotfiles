b9f486d464445fe6a21cbe4a6f480fb63447b2f6
Chad Collins <chadc@allrecipes.com>
2018-05-08 10:15:10 -0700

    adding bootstrap.sh

--- git/bootstrap.sh
+++ git/bootstrap.sh
@@ -1,15 +1,26 @@
 #######
 # Description
-# Bootstraps git config files in bash
+# Bootstraps git config files
 #
 #
 # Ensures $DOTFILES_BASE/git
 #    $HOME/.gitconfig -> $dotfiles/git/gitconfig
-#    $HOME/.gitconfig_os -> $dotfiles/git/gitconfig_os_windows
+#    $HOME/.gitconfig_os -> $dotfiles/git/gitconfig_os_osx
 #    $HOME/.gitignore -> $dotfiles/git/gitignore
 #    $HOME/.gitattributes -> $dotfiles/git/gitattributes
 #
 ######
 
 # symlink $dotfiles/gitconfig to /etc/gitconfig
+echo "Linking gitconfig."
 ln -f ./gitconfig /etc/gitconfig
+
+echo "Linking gitconfig_os"
+ln -f ./gitconfig_os_osx /etc/gitconfig_os
+
+echo "Linking gitignore"
+ln -f ./gitignore /etc/gitignore
+
+echo "Linking gitattributes"
+ln -f ./gitattributes /etc/gitattributes
+
