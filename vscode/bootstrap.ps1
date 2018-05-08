80d4ab258bd7fdb13cffde82253d46555885075e
Chad Collins <chad.c.collins@gmail.com>
2018-05-08 15:09:46 -0700

    formatting

--- vscode/bootstrap.ps1
+++ vscode/bootstrap.ps1
@@ -24,14 +24,12 @@ param(
     [switch]$save,
     [switch]$uninstall
 )
-Begin
-{
+Begin {
     $dotfilesModulePath = Resolve-Path (Join-Path $PSScriptRoot ../WindowsPowerShell/Modules-Dotfiles/Dotfiles/Dotfiles.psm1)
     Import-Module -Name $dotfilesModulePath
     Set-StrictMode -Version Latest
 }
-Process
-{
+Process {
     $ErrorActionPreference = "Stop"
 
     # Maps: AppData/Roaming/Code/User/* -> $dotfiles/VSCode/*
@@ -39,31 +37,28 @@ Process
         (Join-Path "$env:APPDATA\Code\" "User\") = (Join-Path $PSScriptRoot '');
     }
 
-	if ($save)
-	{
-		$source = (Join-Path "$env:APPDATA" "Code\User\")
-		$dest = $PSScriptRoot
+    if ($save) {
+        $source = (Join-Path "$env:APPDATA" "Code\User\")
+        $dest = $PSScriptRoot
 		
-		# Copy all the *.json settings files from [User]\AppData\Roaming\Code\User into .
-		Get-ChildItem $source -Recurse -Include "*.json" -Exclude "meta.json" | New-Item -ItemType File -Path {Join-Path $dest $_.FullName.Substring($source.Length)} -Force
+        # Copy all the *.json settings files from [User]\AppData\Roaming\Code\User into .
+        Get-ChildItem $source -Recurse -Include "*.json" -Exclude "meta.json" | New-Item -ItemType File -Path {Join-Path $dest $_.FullName.Substring($source.Length)} -Force
 
-		# Write out all the install extensions to the 'extensions' file
-		code --list-extensions | Out-File .\extensions -Encoding UTF8
-	}
-    elseif ($uninstall)
-    {
+        # Write out all the install extensions to the 'extensions' file
+        code --list-extensions | Out-File .\extensions -Encoding UTF8
+    }
+    elseif ($uninstall) {
         # Delete the symlinks that exist
         $symlinks.Keys | Where-Object { Test-DotfilesSymlink -Path $_ -Target $symlinks[$_] } | Foreach-Object { $_.Delete() }
     }
-    else
-    {
+    else {
         # Create symlinks
-        $symlinks.Keys | %{ Set-DotfilesSymbolicLink -Path $_ -Target $symlinks[$_] -ErrorAction Stop }
+        $symlinks.Keys | % { Set-DotfilesSymbolicLink -Path $_ -Target $symlinks[$_] -ErrorAction Stop }
 
-	    # Load up the list of extensions from the file
-	    $vscodeExtensions = Get-Content -Path ./extensions
+        # Load up the list of extensions from the file
+        $vscodeExtensions = Get-Content -Path ./extensions
 
-	    # Loop over the list and install each one
+        # Loop over the list and install each one
         $vscodeExtensions | Foreach-Object {
             Write-Verbose "having VSCode install $_"
             code --install-extension $_
